d8f877fdd73ee13bad18406a68e6a0732fc20715
b <1320954152@qq.com>
2017-05-08 16:23:57 +0800

    register login

--- app/src/main/java/com/henu/example/com/wechat/MainActivity.java
+++ app/src/main/java/com/henu/example/com/wechat/MainActivity.java
@@ -1,15 +1,33 @@
 package com.henu.example.com.wechat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    Button login,register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        login=(Button)findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        register=(Button)findViewById(R.id.button_registor);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
