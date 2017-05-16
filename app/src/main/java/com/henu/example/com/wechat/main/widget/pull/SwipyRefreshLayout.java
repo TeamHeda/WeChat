ff3fc369df436a0789df5e6adbbd3b27c90791d3
b <1320954152@qq.com>
2017-05-16 13:49:51 +0800

    界面持续优化

---
+++ app/src/main/java/com/henu/example/com/wechat/main/widget/pull/SwipyRefreshLayout.java
@@ -0,0 +1,19 @@
+package com.henu.example.com.wechat.main.widget.pull;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by b on 2017/5/16.
+ */
+
+public class SwipyRefreshLayout extends ViewGroup{
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+    public SwipyRefreshLayout(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+}
